fc558d7e20f57a02700965e59829c5e8fcff5409
thamma513 <thamma513@gmail.com>
2018-05-08 19:06:43 -0600

    resize background image

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,91 +1,91 @@
-$( document ).ready(function(){
-    var Random=Math.floor(Math.random()*102+8)
-    
-    
+$(document).ready(function () {
+    var Random = Math.floor(Math.random() * 102 + 8)
+
+
     $('#guessingNumber').text(Random);
-    
-    var num1= Math.floor(Math.random()*12+1)
-    var num2= Math.floor(Math.random()*12+1)
-    var num3= Math.floor(Math.random()*12+1)
-    var num4= Math.floor(Math.random()*12+1)
-    
-    var playerTotal= 0; 
-    var wins= 0;
+
+    var num1 = Math.floor(Math.random() * 12 + 1)
+    var num2 = Math.floor(Math.random() * 12 + 1)
+    var num3 = Math.floor(Math.random() * 12 + 1)
+    var num4 = Math.floor(Math.random() * 12 + 1)
+
+    var playerTotal = 0;
+    var wins = 0;
     var losses = 0;
-    
-  
-  $('#wins').text(wins);
-  $('#losses').text(losses);
-  
-  function reset(){
-        Random=Math.floor(Math.random()*102+8);
+
+
+    $('#wins').text(wins);
+    $('#losses').text(losses);
+
+    function reset() {
+        Random = Math.floor(Math.random() * 102 + 8);
         console.log(Random)
         $('#guessingNumber').text(Random);
-        num1= Math.floor(Math.random()*12+1);
-        num2= Math.floor(Math.random()*12+1);
-        num3= Math.floor(Math.random()*12+1);
-        num4= Math.floor(Math.random()*12+1);
-        playerTotal= 0;
+        num1 = Math.floor(Math.random() * 12 + 1);
+        num2 = Math.floor(Math.random() * 12 + 1);
+        num3 = Math.floor(Math.random() * 12 + 1);
+        num4 = Math.floor(Math.random() * 12 + 1);
+        playerTotal = 0;
+        $('#totalScore').html(playerTotal);
+    }
+
+    function winning() {
+        alert("You won!");
+        wins++;
+        $('#wins').text(wins);
+        reset();
+    }
+
+    function loser() {
+        alert("You lose!");
+        losses++;
+        $('#losses').text(losses);
+        reset()
+    }
+    $('.red').on('click', function () {
+        playerTotal = playerTotal + num1;
+        console.log("New playerTotal= " + playerTotal);
         $('#totalScore').text(playerTotal);
-        } 
+        if (playerTotal == Random) {
+            winning();
+        }
+        else if (playerTotal > Random) {
+            loser();
+        }
+    })
+    $('.blue').on('click', function () {
+        playerTotal = playerTotal + num2;
+        console.log("New playerTotal= " + playerTotal);
+        $('#totalScore').text(playerTotal);
+        if (playerTotal == Random) {
+            winning();
+        }
+        else if (playerTotal > Random) {
+            loser();
+        }
+    })
+    $('.yellow').on('click', function () {
+        playerTotal = playerTotal + num3;
+        console.log("New playerTotal= " + playerTotal);
+        $('#totalScore').text(playerTotal);
 
-  function winning(){
-  alert("You won!");
-    wins++; 
-    $('#wins').text(wins);
-    reset();
-  }
+        if (playerTotal == Random) {
+            winning();
+        }
+        else if (playerTotal > Random) {
+            loser();
+        }
+    })
+    $('.green').on('click', function () {
+        playerTotal = playerTotal + num4;
+        console.log("New playerTotal= " + playerTotal);
+        $('#totalScore').text(playerTotal);
 
-  function loser(){
-  alert ("You lose!");
-    losses++;
-    $('#losses').text(losses);
-    reset()
-  }
-    $('.red').on ('click', function(){
-      playerTotal = playerTotal + num1;
-      console.log("New playerTotal= " + playerTotal);
-      $('#totalScore').text(playerTotal); 
-          if (playerTotal == Random){
+        if (playerTotal == Random) {
             winning();
-          }
-          else if ( playerTotal > Random){
+        }
+        else if (playerTotal > Random) {
             loser();
-          }   
-    })  
-    $('.blue').on ('click', function(){
-      playerTotal = playerTotal + num2;
-      console.log("New playerTotal= " + playerTotal);
-      $('#totalScore').text(playerTotal); 
-          if (playerTotal == Random){
-            winning();
-          }
-          else if ( playerTotal > Random){
-            loser();
-          } 
-    })  
-    $('.yellow').on ('click', function(){
-      playerTotal = playerTotal + num3;
-      console.log("New playerTotal= " + playerTotal);
-      $('#totalScore').text(playerTotal);
-  
-            if (playerTotal == Random){
-            winning();
-          }
-          else if ( playerTotal > Random){
-            loser();
-          } 
-    })  
-    $('.green').on ('click', function(){
-      playerTotal = playerTotal + num4;
-      console.log("New playerTotal= " + playerTotal);
-      $('#totalScore').text(playerTotal); 
-        
-            if (playerTotal == Random){
-            winning();
-          }
-          else if ( playerTotal > Random){
-            loser();
-          }
-    });   
-  }); 
+        }
+    });
+}); 
